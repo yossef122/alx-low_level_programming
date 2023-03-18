ef8eeb23f7b4a45cb77a5e70e5ce32a5ceb4c5ba
youssef helmy <yjww179@gmail.com>
2023-03-17 19:27:08 -0700

    last

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+	for(char c ='a';c<='c';c++)
+	{
+		putchar(c);
+	}
+	printf('\n');
+	return(0);
+
+}	
