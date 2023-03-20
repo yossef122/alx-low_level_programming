10b3d766d7af2790ce7e4c5a140da4be0ed6812a
youssef helmy <yjww179@gmail.com>
2023-03-20 16:02:10 -0700

    last

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
